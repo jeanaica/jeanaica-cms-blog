130b0d4714e6ae9ed6e914ef2d16d81f0865ea00
Jeanaica Suplido <sjeanaica@gmail.com>
2022-05-14 15:52:19 +0800

    📦️ Add Firebase

---
+++ frontend/src/firebase/initialize.ts
@@ -0,0 +1,8 @@
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+import firebaseConfig from '../../firebase.config'
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
