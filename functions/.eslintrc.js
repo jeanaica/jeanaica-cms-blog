a93b756b4b2c3018b69f9e0812be4b4d8a36c9f8
Jeanaica Suplido <sjeanaica@gmail.com>
2022-05-14 16:02:43 +0800

    🚨 (Lint): Modify eslint configuration

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,31 +1,37 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'google',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.dev.json'],
-    sourceType: 'module',
-  },
-  ignorePatterns: [
-    '/lib/**/*', // Ignore built files.
-  ],
-  plugins: ['@typescript-eslint', 'import'],
-  rules: {
-    quotes: ['error', 'single'],
-    'quote-props': 'off',
-    'object-curly-spacing': 'off',
-    'comma-dangle': ['error', 'only-multiline'],
-    'import/no-unresolved': 0,
-  },
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "google",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.dev.json"],
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+    },
+    ignorePatterns: [
+        "/lib/**/*", // Ignore built files.
+    ],
+    plugins: ["@typescript-eslint", "import"],
+    rules: {
+        quotes: ["error", "double"],
+        "quote-props": "off",
+        "object-curly-spacing": "off",
+        "comma-dangle": ["error", "only-multiline"],
+        "import/no-unresolved": 0,
+        indent: ["error", 4],
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", {vars: "local"}],
+        "arrow-parens": ["error", "as-needed"],
+        "max-len": ["error", {code: 120, tabWidth: 4}],
+    },
 };
